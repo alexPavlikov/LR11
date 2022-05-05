25547184ea648c776b6721432848ab537c6036f5
Alexander <96076264+alexPavlikov@users.noreply.github.com>
2022-05-05 17:30:38 +0300

    Add files via upload

---
+++ Lab11/Genplot/Form1.cs
@@ -0,0 +1,131 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Genplot
+{
+    public partial class Form1 : Form
+    {
+        struct Simple
+        {
+            public double xx; public double yy; public int ii;
+        };
+        Simple s;
+        FileInfo my_file = new FileInfo(@"C:\Users\admin\Desktop\SCRATCH");
+        BinaryReader fr;
+        double x, y, xmin, xmax, ymin, ymax;
+        double X, Y, Xmin, Xmax, Ymin, Ymax;
+        double fx, fy, f, xC, yC, XC, YC, c1, c2;
+        Graphics dc; Pen p;
+        public Form1()
+        {
+            InitializeComponent();
+            /* Задание границ области вывода по умолчанию */
+            Xmin = 0.2; Xmax = 8.2; Ymin = 0.5; Ymax = 6.5;
+            textBox1.Text = Xmin.ToString();
+            textBox2.Text = Ymin.ToString();
+            textBox3.Text = Xmax.ToString();
+            textBox4.Text = Ymax.ToString();
+            dc = pictureBox1.CreateGraphics();
+            p = new Pen(Brushes.Black, 1);
+        }
+        /* Метод преобразования вещественной координаты X в целую */
+        private int IX(double x)
+        {
+            double xx = x * (pictureBox1.Size.Width / 10.0) + 0.5;
+            return (int)xx;
+        }
+        /* Метод преобразования вещественной координаты Y в целую */
+        private int IY(double y)
+        {
+            double yy = pictureBox1.Size.Height - y *
+
+            (pictureBox1.Size.Height / 7.0) + 0.5;
+
+            return (int)yy;
+        }
+        /* Функция вычерчивания линии (экран 10х7 условн. единиц) */
+        private void Draw(double x1, double y1, double x2, double y2)
+        {
+            Point point1 = new Point(IX(x1), IY(y1));
+            Point point2 = new Point(IX(x2), IY(y2));
+            dc.DrawLine(p, point1, point2);
+        }
+        /* Функция прорисовки меток-уголков области вывода */
+        private void initViewPort(double Xmin, double Ymin,
+        double Xmax, double Ymax)
+
+        {
+            Draw(Xmin, Ymin, Xmin + 0.2, Ymin);
+            Draw(Xmin, Ymin, Xmin, Ymin + 0.2);
+            Draw(Xmin, Ymax, Xmin + 0.2, Ymax);
+            Draw(Xmin, Ymax, Xmin, Ymax - 0.2);
+            Draw(Xmax, Ymin, Xmax - 0.2, Ymin);
+            Draw(Xmax, Ymin, Xmax, Ymin + 0.2);
+            Draw(Xmax, Ymax, Xmax - 0.2, Ymax);
+            Draw(Xmax, Ymax, Xmax, Ymax - 0.2);
+            Draw(Xmax / 2 - 0.2, Ymin, Xmax / 2 + 0.2, Ymin);
+            Draw(Xmax / 2, Ymin, Xmax / 2, Ymin + 0.2);
+        }
+        /* Чтение из файла Scratch и вычерчивание кривой */
+        private void button2_Click(object sender, EventArgs e)
+        {
+            double Xold = 0, Yold = 0;
+            /* Первый проход для определения границ окна xmin, ymin,
+            xmax, ymax */
+            fr = new BinaryReader(my_file.Open(FileMode.Open,FileAccess.Read));
+            while (fr.BaseStream.Position < fr.BaseStream.Length)
+            {
+                s.xx = fr.ReadDouble(); s.yy = fr.ReadDouble();
+                s.ii = fr.ReadInt32();
+                x = s.xx; y = s.yy;
+                if (x < xmin) xmin = x;
+                if (x > xmax) xmax = x;
+                if (y < ymin) ymin = y;
+                if (y > ymax) ymax = y;
+            }
+            fr.Close();
+            /* Вызов функции отображения границ области вывода */
+            initViewPort(Xmin, Ymin, Xmax, Ymax);
+            /* Получение коэффициентов формулы перевода мировых
+            координат в экранные */
+            fx = (Xmax - Xmin) / (xmax - xmin);
+            fy = (Ymax - Ymin) / (ymax - ymin);
+            f = (fx < fy ? fx : fy);
+            xC = 0.5 * (xmin + xmax); yC = 0.5 * (ymin + ymax);
+            XC = 0.5 * (Xmin + Xmax); YC = 0.5 * (Ymin + Ymax);
+            c1 = XC - f * xC; c2 = YC - f * yC;
+            /* Второй проход для вычерчивания */
+            fr = new BinaryReader(my_file.Open(FileMode.Open,FileAccess.Read));
+            while (fr.BaseStream.Position < fr.BaseStream.Length)
+            {
+                s.xx = fr.ReadDouble(); s.yy = fr.ReadDouble();
+                s.ii = fr.ReadInt32();
+                x = s.xx; y = s.yy;
+                X = f * x + c1; Y = f * y + c2;
+                if (s.ii == 1) { Draw(Xold, Yold, X, Y); }
+                Xold = X; Yold = Y;
+            }
+            fr.Close();
+        }
+        /* Очистка области вывода */
+        private void button3_Click(object sender, EventArgs e)
+        { dc.Clear(Color.White); }
+        /* Изменение границ области вывода */
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Xmin = Convert.ToDouble(textBox1.Text);
+            Ymin = Convert.ToDouble(textBox2.Text);
+            Xmax = Convert.ToDouble(textBox3.Text);
+            Ymax = Convert.ToDouble(textBox4.Text);
+            MessageBox.Show("Координаты области вывода изменены");
+        }
+    }
+}
---
+++ Lab11/Graphics Lab 11/Form1.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Graphics_Lab_11
+{
+    public partial class Form1 : Form
+    {
+        struct Simple
+        {
+            public double xx; public double yy; public int ii;
+        };
+        Simple s;
+        FileInfo my_file = new FileInfo(@"C:\Users\admin\Desktop\SCRATCH");
+        BinaryWriter fw;
+        public Form1()
+        { InitializeComponent(); }
+        /* Создание файла Scratch и открытие его на запись */
+        void pfopen()
+        {
+            fw = new BinaryWriter(my_file.Open(FileMode.Create,FileAccess.Write));
+
+        }
+        /* Запись в файл точки с флагом перемещения */
+        void pmove(double x, double y)
+        {
+            s.xx = x; s.yy = y; s.ii = 0;
+            fw.Write(s.xx); fw.Write(s.yy); fw.Write(s.ii);
+        }
+        /* Запись в файл точки с флагом рисования */
+        void pdraw(double x, double y)
+        {
+            s.xx = x; s.yy = y; s.ii = 1;
+            fw.Write(s.xx); fw.Write(s.yy); fw.Write(s.ii);
+        }
+        /* Закрытие файла */
+        void pfclose()
+        { fw.Close(); }
+        /* Главная функция генерации квадрата и треугольника */
+        void square_and_triangle(int n, double x0, double y0,double a, double phi)
+        {
+            double[] x = new double[5]; double[] y = new double[5];
+            double[] xx = new double[5]; double[] yy = new double[5];
+            double cphi, sphi, c1, c2, b, c, alpha, calpha, salpha;
+            int i;
+            if (n == 0) return;
+            /* углы phi и alpha в радианах, угол delta в градусах */
+            alpha = (45 + int.Parse(textBox3.Text)) * Math.PI / 180.0;
+            x[0] = x[3] = x0; x[1] = x[2] = x0 + a;
+            y[0] = y[1] = y0; y[2] = y[3] = y0 + a;
+            calpha = Math.Cos(alpha); salpha = Math.Sin(alpha);
+            c = a * calpha; b = a * salpha;
+            x[4] = x[3] + c * calpha;
+            y[4] = y[3] + c * salpha;
+            /* Поворот вокруг точки (x0, y0) на угол phi;*/
+            cphi = Math.Cos(phi); sphi = Math.Sin(phi);
+            c1 = x0 - x0 * cphi + y0 * sphi;
+            c2 = y0 - x0 * sphi - y0 * cphi;
+            for (i = 0; i < 5; i++)
+            {
+                xx[i] = x[i] * cphi - y[i] * sphi + c1;
+                yy[i] = x[i] * sphi + y[i] * cphi + c2;
+            }
+            pmove(xx[3], yy[3]);
+            for (i = 0; i < 5; i++) pdraw(xx[i], yy[i]);
+            pdraw(xx[2], yy[2]);
+            square_and_triangle(n - 1, xx[3], yy[3], c, phi + alpha);
+            square_and_triangle(n - 1, xx[4], yy[4], b, phi + alpha -
+            0.5 * Math.PI);
+            phi = double.Parse(textBox1.Text);
+        }
+        /* Вызов функции генерации дерева Пифагора */
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (textBox1.Text == "" || textBox2.Text == "" || textBox3.Text == "")
+                {
+                    throw new Exception();
+                }
+                int n = int.Parse(textBox2.Text);
+                pfopen();
+                square_and_triangle(n, 0.0, 0.0, 1.0, 0.0);
+                pfclose();
+                MessageBox.Show("Дерево Пифагора записано в файл Scratch");
+                
+            }
+            catch (Exception) { MessageBox.Show("Один из параметров не задан!"); }
+        }
+    }
+ }
